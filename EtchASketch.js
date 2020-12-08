5f3a7996608fbb02ed4b18222aa713fefbe4d0f3
lc-code20 <62159627+lc-code20@users.noreply.github.com>
2020-12-08 16:50:47 +0000

    add new functionality, prompt for new size, reset button, mouseover effect

--- EtchASketch.js
+++ EtchASketch.js
@@ -2,7 +2,16 @@ const container = document.querySelector('#container');
 
 const grid = document.createElement('div');
 grid.classList.add('grid');
+
+const reset = document.createElement('input');
+reset.type = "button";
+reset.className = "btn btn-primary";
+reset.value = "Reset";
+container.appendChild(reset);
+
 container.appendChild(grid);
+
+
 
 createGrid(16);
 
@@ -15,5 +24,36 @@ function createGrid(s){
 
         grid.appendChild(squares);
 
+        squares.addEventListener("mouseover", function(e){     
+            e.target.style.background = 'grey';
+        });
     }
+
+    return s;
 }
+
+reset.addEventListener('click', resetGrid);
+
+
+function resetGrid(){
+
+    console.log("check");
+     const squaresS = Array.from(document.querySelectorAll('.grid-item'));
+
+     squaresS.forEach(sq => sq.remove());
+
+    createGrid(promptNew());
+}
+
+function promptNew(){
+    //var txt;
+    //newSize = prompt("Please input new size of square: ");
+    newSize = prompt("Please input new size of square: ");
+
+    while(newSize!=null && newSize>64){
+        newSize = prompt("Please input new size of square: ");
+    }
+
+    return newSize;
+}
+
